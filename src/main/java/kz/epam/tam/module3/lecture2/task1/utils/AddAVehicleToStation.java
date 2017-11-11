23e0ccf7111eeb42fd847e2a61744ad89bca3b3d
Yuriy_Jurayev <yuriy_jurayev@epam.com>
2017-11-11 08:33:24 +0600

    modified path to resourse files

--- src/main/java/kz/epam/tam/module3/lecture2/task1/utils/AddAVehicleToStation.java
+++ src/main/java/kz/epam/tam/module3/lecture2/task1/utils/AddAVehicleToStation.java
@@ -2,32 +2,43 @@ package kz.epam.tam.module3.lecture2.task1.utils;
 
 import kz.epam.tam.module3.lecture2.task1.Car;
 import kz.epam.tam.module3.lecture2.task1.Van;
-import kz.epam.tam.module3.lecture2.task1.data.parser.TxtFileReader;
+import kz.epam.tam.module3.lecture2.task1.data.reader.TxtFileReader;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class AddAVehicleToStation {
-    private  static final String ABS_PATH_TO_CAR_TXT = "D:\\JavaProjects\\TaxiStation\\car.txt";
-    private  static final String ABS_PATH_TO_VAN_TXT = "D:\\JavaProjects\\TaxiStation\\van.txt";
-    private static Scanner sc = new Scanner(System.in);
+    private  static final String PATH_TO_CAR_TXT = "src\\main\\resources\\car.txt";
+    private  static final String PATH_TO_VAN_TXT = "src\\main\\resources\\van.txt";
 
     public static List<Car> fillBaseCharacteristicsCar(){
         List<Car> carList  = new ArrayList<>();
         TxtFileReader data = new TxtFileReader();
-        String[][] info = data.readFile(ABS_PATH_TO_CAR_TXT);
+        String[][] info = data.readFile(PATH_TO_CAR_TXT);
         for(int i =0; i < info.length; i++){
-        carList.add(new Car(info[i][0],Long.parseLong(info[i][1]),Integer.parseInt(info[i][2]),info[i][3]));
+            try {
+                carList.add(new Car(info[i][0], Long.parseLong(info[i][1]), Integer.parseInt(info[i][2]), info[i][3]));
+            }catch (NumberFormatException e){
+                carList.add(new Car(info[i][0],Long.parseLong(info[i][1].replaceAll("[^0-9]", "")),
+                        Integer.parseInt(info[i][2].replaceAll("[^0-9]", "")),info[i][3]));
+            }
         }
         return carList;
     }
+
     public static List<Van> fillBaseCharacteristicsVan(){
         TxtFileReader data = new TxtFileReader();
         List<Van> vanList  = new ArrayList<>();
-        String[][] info = data.readFile(ABS_PATH_TO_VAN_TXT);
+        String[][] info = data.readFile(PATH_TO_VAN_TXT);
         for(int i =0; i < info.length; i++){
+            try{
             vanList.add(new Van(info[i][0],Long.parseLong(info[i][1]),Integer.parseInt(info[i][2]),Integer.parseInt(info[i][3]),info[i][4]));
+            }catch (NumberFormatException e){
+                vanList.add(new Van(info[i][0],Long.parseLong(info[i][1].replaceAll("[^0-9]", "")),
+                        Integer.parseInt(info[i][2].replaceAll("[^0-9]", "")),
+                        Integer.parseInt(info[i][3].replaceAll("[^0-9]", "")), info[i][4]));
+            }
         }
         return vanList;
     }
