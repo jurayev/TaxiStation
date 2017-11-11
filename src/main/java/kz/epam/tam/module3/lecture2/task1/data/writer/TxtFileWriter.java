2a1a7c748c304dc5ad9e3e39e814750d12a94fcb
Yuriy_Jurayev <yuriy_jurayev@epam.com>
2017-11-11 08:31:10 +0600

    modified data to write

--- src/main/java/kz/epam/tam/module3/lecture2/task1/data/writer/TxtFileWriter.java
+++ src/main/java/kz/epam/tam/module3/lecture2/task1/data/writer/TxtFileWriter.java
@@ -3,22 +3,23 @@ package kz.epam.tam.module3.lecture2.task1.data.writer;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.List;
 
 public class TxtFileWriter {
+    private static final String PATH_TO_RESULTS_TXT = "src\\main\\resources\\results.txt";
 
-    public void writeToFile() {
-
-
-        String str;
+    public void writeToFile(long countCar, List<String> sortedByConsumptionList, String search) {
         try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter("D:\\JavaProjects\\TaxiStation\\results.txt"));
-            String text = "ashdaysdy\n213\nsjdjasdjkasjdn\nasdhahsd";
-            writer.write(text);
+            BufferedWriter writer = new BufferedWriter(new FileWriter(PATH_TO_RESULTS_TXT));
+            writer.write("All results are recorded" + "\nBudget is " + countCar + ";\nSort By fuel consumption:\n" + sortedByConsumptionList
+                    + ";\n" + search);
+            writer.newLine();
             writer.flush();
         }catch (IOException e){
             System.out.println(e.getMessage());
         }
     }
 }
+
 
 
