8490530ecbfd8ff0e11cfc470ac6469d1e264567
Yuriy_Jurayev <yuriy_jurayev@epam.com>
2017-11-21 12:47:53 +0600

    rewrote collections comparison model

---
+++ src/main/java/kz/epam/tam/module3/lecture234/collectionscomparison/utils/PerformanceHelper.java
@@ -0,0 +1,50 @@
+package kz.epam.tam.module3.lecture234.collectionscomparison.utils;
+
+public class PerformanceHelper {
+
+    public void compareListsPerformance(){
+        ListPerformance listPerformance = new ListPerformance();
+        System.out.println("Performance measurement is started!");
+        System.out.println("==================================================");
+        System.out.println("Compare List's performance!");
+        System.out.println("1 - Adding items to a list");
+        listPerformance.checkAdditionPerformanceOfArrayL();
+        listPerformance.checkAdditionPerfomanceOfLinkedL();
+        System.out.println("2 - Searching items in a list");
+        listPerformance.checkSearchPerformanceOfArrayL();
+        listPerformance.checkSearchPerformanceOfLinkedL();
+        System.out.println("3 - Deleting items from a list");
+        listPerformance.checkDeletionPerformanceOfArrayL();
+        listPerformance.checkDeletionPerformanceOfLinkedL();
+        System.out.println("==================================================");
+    }
+    public void compareSetsPerformance(){
+        SetPerformance setPerformance = new SetPerformance();
+        System.out.println("Compare Set's performance!");
+        System.out.println("1 - Adding items to a set");
+        setPerformance.checkAdditionPerformanceOfHashSet();
+        setPerformance.checkAdditionPerformanceOfTreeSet();
+        System.out.println("2 - Searching items in a set");
+        setPerformance.checkSearchPerformanceOfHashSet();
+        setPerformance.checkSearchPerformanceOfTreeSet();
+        System.out.println("3 - Deleting items from a set");
+        setPerformance.checkDeletionPerformanceOfHashSet();
+        setPerformance.checkDeletionPerformanceOfTreeSet();
+        System.out.println("==================================================");
+    }
+    public void compareMapsPerformance(){
+        MapPerformance mapPerformance = new MapPerformance();
+        System.out.println("Compare Map's performance!");
+        System.out.println("1 - Adding items in a map");
+        mapPerformance.checkAdditionPerformanceOfHashMap();
+        mapPerformance.checkAdditionPerformanceOfTreeMap();
+        System.out.println("2 - Searching items in a map");
+        mapPerformance.checkSearchPerformanceOfHashMap();
+        mapPerformance.checkSearchPerformanceOfTreeMap();
+        System.out.println("3 - Deleting items from a map");
+        mapPerformance.checkDeletionPerformanceOfHashMap();
+        mapPerformance.checkDeletionPerformanceOfTreeMap();
+        System.out.println("==================================================");
+    }
+
+}
