64f60945d801390ca29f0dd802c769da24059fba
Yuriy_Jurayev <yuriy_jurayev@epam.com>
2017-12-15 16:42:46 +0600

    added global constants class to store project constants

---
+++ src/main/java/kz/epam/tam/module3/lecture234/taxistation/property/GlobalConstants.java
@@ -0,0 +1,7 @@
+package kz.epam.tam.module3.lecture234.taxistation.property;
+
+public class GlobalConstants {
+
+    public static final String TAXISTATION_PROPERTIES_PATH = "src\\main\\resources\\taxistation.properties";
+}
+
