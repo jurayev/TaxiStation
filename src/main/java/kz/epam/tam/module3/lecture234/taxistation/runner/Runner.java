503da51a24c07ceb255b8ed30ea0ebe330313017
Yuriy_Jurayev <yuriy_jurayev@epam.com>
2017-12-15 16:43:59 +0600

    renamed instance variable name

--- src/main/java/kz/epam/tam/module3/lecture234/taxistation/runner/Runner.java
+++ src/main/java/kz/epam/tam/module3/lecture234/taxistation/runner/Runner.java
@@ -8,7 +8,7 @@ public class Runner {
 
     public static void main(String[] args) {
 
-        TaxiStationManipulator creator = new TaxiStationManipulator();
-        creator.manipulateTaxiStationMainFeatures();
+        TaxiStationManipulator manipulator = new TaxiStationManipulator();
+        manipulator.manipulateTaxiStationMainFeatures();
     }
 }
